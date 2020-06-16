f45a1037c786765633ec4613e5e1e8a47d10b556
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-06-15 19:53:10 -0500

    find post

--- src/main/java/sw2/lab6/teletok/repository/PostCommentRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/PostCommentRepository.java
@@ -2,8 +2,12 @@ package sw2.lab6.teletok.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sw2.lab6.teletok.entity.Post;
 import sw2.lab6.teletok.entity.PostComment;
+
+import java.util.List;
 
 @Repository
 public interface PostCommentRepository extends JpaRepository<PostComment, Integer> {
+    List<PostComment> findPostCommentByPost(Post post);
 }
--- src/main/java/sw2/lab6/teletok/repository/PostLikeRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/PostLikeRepository.java
@@ -2,8 +2,13 @@ package sw2.lab6.teletok.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.entity.PostComment;
 import sw2.lab6.teletok.entity.PostLike;
+
+import java.util.List;
 
 @Repository
 public interface PostLikeRepository extends JpaRepository<PostLike, Integer> {
+    List<PostComment> findPostLikeByPost(Post post);
 }
