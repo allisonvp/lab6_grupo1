244e7123196c9ab340ced21b0b8f85196a0ca2f9
allisonvp <a.velardep@pucp.edu.pe>
2020-06-15 23:41:34 -0500

    post

--- src/main/java/sw2/lab6/teletok/repository/PostCommentRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/PostCommentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import sw2.lab6.teletok.Dto.CantidadComentariosDto;
 import sw2.lab6.teletok.entity.Post;
 import sw2.lab6.teletok.entity.PostComment;
 
@@ -17,7 +18,7 @@ public interface PostCommentRepository extends JpaRepository<PostComment, Intege
             "INNER JOIN post p ON p.id=pc.post_id\n" +
             "INNER JOIN user u ON u.id=p.user_id\n" +
             "group by pc.post_id",nativeQuery = true)
-    int obtenerCantComentPorPost();
+    List<CantidadComentariosDto> obtenerCantComentPorPost();
 
 
 
--- src/main/java/sw2/lab6/teletok/repository/PostLikeRepository.java
+++ src/main/java/sw2/lab6/teletok/repository/PostLikeRepository.java
@@ -3,6 +3,7 @@ package sw2.lab6.teletok.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import sw2.lab6.teletok.Dto.CantidadLikesDto;
 import sw2.lab6.teletok.entity.Post;
 import sw2.lab6.teletok.entity.PostComment;
 import sw2.lab6.teletok.entity.PostLike;
@@ -14,11 +15,12 @@ import java.util.List;
 public interface PostLikeRepository extends JpaRepository<PostLike, Integer> {
 
     @Query(value="SELECT count(*) as cantlikes, pl.post_id as postid, p.description as descrip, p.creation_date as dia, p.media_url as foto, u.username as usuario\n" +
-            "            FROM post_like pl \n" +
+            "   FROM post_like pl \n" +
             "            INNER JOIN post p ON p.id=pl.post_id\n" +
             "            INNER JOIN user u ON u.id=p.user_id\n" +
             "            group by pl.post_id",nativeQuery = true)
-    int obtenerCantLikesPorPost();
+
+    List<CantidadLikesDto> obtenerCantLikesPorPost();
 
     List<PostLike> findPostLikeByPost(Post post);
     List<PostLike> findPostLikeByPostAndUser(Post post, User user);
