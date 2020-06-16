3f1632c5d10cbcb5afc8bee34da168e6a7dfb966
katty <a20160555@pucp.edu.pe>
2020-06-15 20:37:39 -0500

    commit katty

--- src/main/java/sw2/lab6/teletok/controller/PostController.java
+++ src/main/java/sw2/lab6/teletok/controller/PostController.java
@@ -1,23 +1,73 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.entity.User;
+import sw2.lab6.teletok.repository.PostRepository;
+import sw2.lab6.teletok.utils.UploadObject;
+
+import javax.validation.Valid;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 @Controller
 public class PostController {
 
+    @Autowired
+    PostRepository postRepository;
+
+
     @GetMapping(value = {"", "/"})
-    public String listPost(){
+    public String listPost() {
+
         return "post/list";
     }
 
     @GetMapping("/post/new")
-    public String newPost(){
+    public String newPost(@ModelAttribute("post") Post post) {
         return "post/new";
     }
 
     @PostMapping("/post/save")
-    public String savePost() {
+    public String savePost(@ModelAttribute("post") @Valid Post post,
+                           BindingResult bindingResult,
+                           RedirectAttributes attr,
+                           @RequestParam("user") int user,
+                           @RequestParam("photo") MultipartFile multipartFile) {
+
+        if (!post.getDescription().isEmpty()) {
+            if (post.getDescription().trim().length() < 3) {
+                bindingResult.rejectValue("nombre", "error.user", "La descripción debe tener más de 3 letras.");
+            }
+            if (post.getDescription().trim().length() > 45) {
+                bindingResult.rejectValue("nombre", "error.user", "La descripción debe tener menos de 45 letras.");
+            }
+            if (post.getDescription().trim().length() == 0) {
+                bindingResult.rejectValue("nombre", "error.user", "Ingrese una descripción.");
+            }
+        }
+
+        if (post.getMediaUrl().isEmpty()) {
+            bindingResult.rejectValue("mediaUrl", "error.user", "Debe colocar una foto.");
+        }
+
+
+        if (bindingResult.hasErrors()) {
+            return "post/new";
+        } else if (post.getId() == 0) {
+            post.setCreationDate(LocalDateTime.now());
+            post.getUser().setId(user);
+            UploadObject.uploadProfilePhoto(post,multipartFile);
+            attr.addFlashAttribute("msg", "Post creado exitosamente");
+            postRepository.save(post);
+        }
         return "redirect:/";
     }
 
